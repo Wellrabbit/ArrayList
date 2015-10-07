7f6c79d968cdbbdd4667b21fd738c87c95228ac2
Emily Orton <eort3611@740s-pl-106.local>
2015-10-07 13:54:20 -0600

    added Documentation

--- src/lists/controller/Controller.java
+++ src/lists/controller/Controller.java
@@ -12,18 +12,23 @@ public class Controller
 		firstWords = new ArrayList<String>();
 		popupDisplay = new ListViewer();
 	}
-	
+	/**
+	 * Add things to the first and second position of the array
+	 */
 	public void Start()
 	{
 		firstWords.add("fjakhfjkdghakdfjggjkreahfdk");
+		firstWords.add("BAbbabbaaababbaaaaa");
 		showList();
 	}
-	
+	/**
+	 * Display in popup the arrays stored strings via spot
+	 */
 	private void showList()
 	{
 		for(int spot = 0; spot < firstWords.size(); spot++)
 		{
-			popupDisplay.showList("The Contents at " + spot + "are" + firstWords.get(spot));
+			popupDisplay.showList("The Contents at " + spot + " are " + firstWords.get(spot));
 		}
 	}
 	
