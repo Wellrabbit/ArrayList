5936ddd8f290804590ee57c09b05fc33872c78e8
Emily Orton <eort3611@740s-pl-106.local>
2015-10-07 13:48:42 -0600

    Write string to popup

--- src/lists/controller/Controller.java
+++ src/lists/controller/Controller.java
@@ -1,18 +1,30 @@
 package lists.controller;
 import java.util.ArrayList;
+import list.view.ListViewer;
 
 public class Controller 
 {
 	private ArrayList<String> firstWords;
+	private ListViewer popupDisplay;
 	
 	public Controller()
 	{
 		firstWords = new ArrayList<String>();
+		popupDisplay = new ListViewer();
 	}
 	
 	public void Start()
 	{
 		firstWords.add("fjakhfjkdghakdfjggjkreahfdk");
+		showList();
 	}
-
+	
+	private void showList()
+	{
+		for(int spot = 0; spot < firstWords.size(); spot++)
+		{
+			popupDisplay.showList("The Contents at " + spot + "are" + firstWords.get(spot));
+		}
+	}
+	
 }
